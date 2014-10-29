a25e938ea148ef5670d927b4e88d4ab04f0db941
Kay Zheng <l04m33@gmail.com>
2014-10-29 10:17:01 +0800

    Add oh-my-zsh files

---
+++ .oh-my-zsh_custom/plugins/my-env/my-env.plugin.zsh
@@ -0,0 +1,24 @@
+APPINST_PATH="$HOME/app_inst"
+
+if [ -d "$APPINST_PATH" ]; then
+    export APPINST_PATH
+    APPINST_BINS=$(echo $APPINST_PATH/*/bin | sed 's/[ \t]\+/:/g')
+fi
+if [ -n "$APPINST_BINS" ]; then
+    export PATH=$APPINST_BINS:$PATH
+fi
+
+# virtualenvwrapper
+if which virtualenvwrapper.sh > /dev/null 2>&1; then
+    export WORKON_HOME=$HOME/.virtualenvs
+    if which python3 > /dev/null 2>&1; then
+        export VIRTUALENVWRAPPER_PYTHON="$(which python3)"
+        export VIRTUALENVWRAPPER_VIRTUALENV="$(which pyvenv)"
+    fi
+
+    if [ ! -d "$WORKON_HOME" ]; then
+        mkdir -p "$WORKON_HOME"
+    fi
+
+    source $(which virtualenvwrapper.sh)
+fi
