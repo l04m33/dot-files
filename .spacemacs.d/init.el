75a481435587a5411f300f434b4bfbf98f64941d
Kay Zheng <l04m33@gmail.com>
2016-08-28 23:33:21 +0800

    auto-completion layer

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -17,25 +17,10 @@ values."
    ;; List of configuration layers to load. If it is the symbol `all' instead
    ;; of a list then all discovered layers will be installed.
    dotspacemacs-configuration-layers
-   '(
-     ;; ----------------------------------------------------------------
-     ;; Example of useful layers you may want to use right away.
-     ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
-     ;; <M-m f e R> (Emacs style) to install them.
-     ;; ----------------------------------------------------------------
-     ;; auto-completion
-     ;; better-defaults
+   '(auto-completion
+     version-control
      emacs-lisp
      common-lisp
-     ;; git
-     ;; markdown
-     ;; org
-     ;; (shell :variables
-     ;;        shell-default-height 30
-     ;;        shell-default-position 'bottom)
-     ;; spell-checking
-     ;; syntax-checking
-     version-control
      ql-env)
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
