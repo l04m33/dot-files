5c66d326f70a39b94a43738128a502a743681082
Kay Z <l04m33@gmail.com>
2017-10-11 10:41:41 +0800

    Use tr command for compatibility with FreeBSD

--- .oh-my-zsh_custom/plugins/my-env/my-env.plugin.zsh
+++ .oh-my-zsh_custom/plugins/my-env/my-env.plugin.zsh
@@ -5,7 +5,7 @@ ROSWELL_PATH="$HOME/.roswell"
 
 if [ -d "$APPINST_PATH" ]; then
     export APPINST_PATH
-    APPINST_BINS=$(echo $APPINST_PATH/*/bin | sed 's/[ \t]\+/:/g')
+    APPINST_BINS=$(echo $APPINST_PATH/*/bin | tr -s '[:blank:]' ':')
 fi
 if [ -n "$APPINST_BINS" ]; then
     export PATH=$APPINST_BINS:$PATH
