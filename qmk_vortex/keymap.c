dd5ee32fa70f8868bb6903ea284bdaea046fc4d5
Kay Z <l04m33@gmail.com>
2019-02-20 17:30:54 +0800

    Keep the right Shift key for now

--- qmk_vortex/keymap.c
+++ qmk_vortex/keymap.c
@@ -27,7 +27,7 @@ const uint16_t keymaps_default[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_core(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(2),
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT, MO(2),
         KC_ESC,  KC_LGUI, KC_LALT, KC_BSPC, KC_SPC,  KC_SPC,  MO(1),   KC_RALT, KC_APP,  KC_RCTL
     ),
     [1] = LAYOUT_core(
