04797f5a1563197df856394b1f577c31d6ca1406
Kay Z <l04m33@gmail.com>
2019-02-22 22:56:11 +0800

    Dynamic macros for the Core

--- qmk_vortex/keymap.c
+++ qmk_vortex/keymap.c
@@ -22,6 +22,13 @@
 
 #include "vortex.h"
 
+enum keycodes {
+    DUMMY = SAFE_RANGE,
+    DYNAMIC_MACRO_RANGE,
+};
+
+#include "dynamic_macro.h"
+
 /************************************************
  * keymaps
  ************************************************/
@@ -44,10 +51,10 @@ const uint16_t keymaps_default[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     /* Fn1 layer */
     [2] = LAYOUT_core(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,
-        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
-        _______, _______, _______, _______, _______, KC_QUOT, KC_SLSH, KC_LBRC, KC_RBRC, KC_BSLS, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+        KC_GRV,          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,          KC_F7,        KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,
+        KC_1,            KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,           KC_8,         KC_9,    KC_0,    KC_MINS, KC_EQL,
+        _______,         _______, _______, _______, _______, KC_QUOT, KC_SLSH,        KC_LBRC,      KC_RBRC, KC_BSLS, _______, _______,
+        DYN_MACRO_PLAY1, _______, _______, _______, _______, _______, DYN_REC_START1, DYN_REC_STOP, _______, _______
     ),
     /* Space Fn layer */
     /* XXX: mouse keys are not working due to lack of endpoints (?) */
@@ -340,6 +347,9 @@ void matrix_scan_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!process_record_dynamic_macro(keycode, record)) {
+        return false;
+    }
     return true;
 }
 
