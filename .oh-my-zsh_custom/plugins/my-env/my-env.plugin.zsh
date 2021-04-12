32f3e29a6cb630e0c4af955dcb20b32b73e544e1
Kay Z <l04m33@gmail.com>
2021-04-12 23:26:56 +0800

    Make the shell env available in .zshenv by sourcing the global /etc/profile file

--- .oh-my-zsh_custom/plugins/my-env/my-env.plugin.zsh
+++ .oh-my-zsh_custom/plugins/my-env/my-env.plugin.zsh
@@ -8,21 +8,4 @@ if [ -n "$APPINST_BINS" ]; then
     export PATH=$APPINST_BINS:$PATH
 fi
 
-GUIX_ENV_FILE="$HOME/.config/guix/current/etc/profile"
-GUIX_PROFILE_ENV_FILE="$HOME/.guix-profile/etc/profile"
-for ef in "$GUIX_ENV_FILE" "$GUIX_PROFILE_ENV_FILE"; do
-    if [ -f "$ef" ]; then
-        export GUIX_PROFILE=${ef:h:h}
-        source "$ef"
-    fi
-done
-if [ -n "$GUIX_PROFILE" ]; then
-    export GUIX_LOCPATH="$GUIX_PROFILE/lib/locale"
-    if [ -f "$GUIX_PROFILE/lib/libclang.so" ]; then
-        export LIBCLANG_PATH="$GUIX_PROFILE/lib"
-    fi
-fi
-
 export EDITOR=$(if which kak > /dev/null 2>&1; then echo kak; else echo vim; fi)
-
-# Aliases
--- .zshenv
+++ .zshenv
@@ -1,1 +1,10 @@
-source $HOME/.dot-files/.oh-my-zsh_custom/plugins/my-env/my-env.plugin.zsh
+if [ -z "$__ZSH_ENV_INITIALIZED__" ]; then
+    GLOBAL_ETC_PROFILE="/etc/profile"
+    if [ -f "$GLOBAL_ETC_PROFILE" ]; then
+        source "$GLOBAL_ETC_PROFILE"
+    fi
+
+    source $HOME/.dot-files/.oh-my-zsh_custom/plugins/my-env/my-env.plugin.zsh
+
+    export __ZSH_ENV_INITIALIZED__=y
+fi
