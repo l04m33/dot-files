6fee9544229e834867d9f0c05974c6006ea9d299
Kay Z <l04m33@gmail.com>
2021-04-29 01:32:59 +0800

    load_guix_profile

--- .oh-my-zsh_custom/plugins/my-env/my-env.plugin.zsh
+++ .oh-my-zsh_custom/plugins/my-env/my-env.plugin.zsh
@@ -9,3 +9,20 @@ if [ -n "$APPINST_BINS" ]; then
 fi
 
 export EDITOR=$(if which kak > /dev/null 2>&1; then echo kak; else echo vim; fi)
+
+function load_guix_profile {
+    if [ $# -gt 0 ]; then
+        local pf_name=$1; shift
+        local GUIX_PROFILE="${HOME}/.adhoc-profiles/${pf_name}"
+        if [ -f "${GUIX_PROFILE}/etc/profile" ]; then
+            source "${GUIX_PROFILE}/etc/profile"
+            printf "Profile loaded: %s\n" "${GUIX_PROFILE}"
+        else
+            printf "Not a loadable profile: %s\n" "${GUIX_PROFILE}" 1>&2
+        fi
+    else
+        printf "No profile specified.\n" 1>&2
+    fi
+}
+
+alias lgp=load_guix_profile
