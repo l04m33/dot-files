30ce6650199c358b9d11a2a5a1efa95727a3e15b
Kay Z <l04m33@gmail.com>
2021-04-29 23:10:40 +0800

    Load multiple profiles in a batch

--- .oh-my-zsh_custom/plugins/my-functions/my-functions.plugin.zsh
+++ .oh-my-zsh_custom/plugins/my-functions/my-functions.plugin.zsh
@@ -1,13 +1,14 @@
 function load_guix_profile {
     if [ $# -gt 0 ]; then
-        local pf_name=$1; shift
-        local GUIX_PROFILE="${HOME}/.adhoc-profiles/${pf_name}"
-        if [ -f "${GUIX_PROFILE}/etc/profile" ]; then
-            source "${GUIX_PROFILE}/etc/profile"
-            printf "Profile loaded: %s\n" "${GUIX_PROFILE}"
-        else
-            printf "Not a loadable profile: %s\n" "${GUIX_PROFILE}" 1>&2
-        fi
+        for pf_name in $@; do
+            local GUIX_PROFILE="${HOME}/.adhoc-profiles/${pf_name}"
+            if [ -f "${GUIX_PROFILE}/etc/profile" ]; then
+                source "${GUIX_PROFILE}/etc/profile"
+                printf "Profile loaded: %s\n" "${GUIX_PROFILE}"
+            else
+                printf "Not a loadable profile: %s\n" "${GUIX_PROFILE}" 1>&2
+            fi
+        done
     else
         printf "No profile specified.\n" 1>&2
     fi
