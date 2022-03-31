ebda311c86efbe5eebcbfaff89b3794723dfd733
Kay Z <l04m33@gmail.com>
2022-03-31 18:30:53 +0800

    .local_env

--- .oh-my-zsh_custom/plugins/my-env/my-env.plugin.zsh
+++ .oh-my-zsh_custom/plugins/my-env/my-env.plugin.zsh
@@ -9,3 +9,7 @@ if [ -n "$APPINST_BINS" ]; then
 fi
 
 export EDITOR=$(if which kak > /dev/null 2>&1; then echo kak; else echo vim; fi)
+
+if [ -e "$HOME/.local_env" ]; then
+    . "$HOME/.local_env"
+fi
